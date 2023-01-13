5097f66ed4b5e3ee9a0a7ea7e5fb8ced18b00422
e-haabre <hvdbreimo@gamil.com>
2023-01-13 10:16:38 +0100

    Spilleranimasjon

--- grafikk.py
+++ grafikk.py
@@ -10,20 +10,41 @@ piratskip1_path = str(grafikk_path) + '\Grafikk\Piratskip1.png'
 piratskip0_path = str(grafikk_path) + '\Grafikk\Piratskip0.png'
 piratskipm1_path = str(grafikk_path) + '\Grafikk\Piratskip-1.png'
 
-black = (0,0,0)
-window_width = 1000
-window_height = 600
-fps = 30
+
+
+anicount = 0
+
+
+
+color_ocean = (0,30,127)
+window_width = 1500
+window_height = 850
+fps = 28
 
 pygame.init()
 window = pygame.display.set_mode((window_width, window_height))
 clock = pygame.time.Clock()
 
-print(ball_path)
+class Spillerbåt:
+    def __init__(self, animasjon, sprite=0):
+        self.ani = animasjon           
+        self.idle_boat = [pygame.image.load(piratskip1_path), pygame.image.load(piratskip0_path), pygame.image.load(piratskipm1_path),pygame.image.load(piratskip0_path)]
+        self.idle_boat = [pygame.transform.scale(self.idle_boat[0], (120,120)),pygame.transform.scale(self.idle_boat[1], (120,120)),pygame.transform.scale(self.idle_boat[2], (120,120)),pygame.transform.scale(self.idle_boat[3], (120,120))]
+        
+        
+    
+    def idle(self):
+        #window.blit(self.idle_boat[self.ani], (100,200))
+        
+        window.blit(self.idle_boat[self.ani//18], (350,250))
+                  
+        
 
-ball_bilde = pygame.image.load(ball_path)
-idle_boat = [pygame.image.load(piratskip1_path), pygame.image.load(piratskip0_path), pygame.image.load(piratskipm1_path)]
-idle_boat = pygame.transform.scale(idle_boat, (80,80))
+
+
+#ball_bilde = pygame.image.load(ball_path)
+
+
 
 while True:
     for event in pygame.event.get():
@@ -32,10 +53,15 @@ while True:
             sys.exit()
 
 
-    window.fill(black)
+    window.fill(color_ocean)
 
-    window.blit(idle_boat,(100,200))
+    #window.blit(pygame.image.load(piratskip1_path),(100,200))
 
-    pygame.display.update()
 
     clock.tick(fps)
+
+    Spillerbåt(anicount).idle()
+    anicount +=1
+    if anicount >= 72:
+        anicount=0
+    pygame.display.update()
