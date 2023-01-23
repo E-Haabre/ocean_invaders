1df67c9c3686064534bb5e6aa5b0c4adef4ca389
e-haabre <hvdbreimo@gamil.com>
2023-01-23 10:48:48 +0100

    Oppdatert

--- grafikk.py
+++ grafikk.py
@@ -1,108 +1,51 @@
 import pygame
 from pygame.locals import *
-import sys
 from sprites import *
 
 count = 0
 runde = 0
 
-color_ocean = (118,200,213)
 window_width = 1500
 window_height = 850
-fps = 28
 
-pygame.init()
 WINDOW = pygame.display.set_mode((window_width, window_height))
-CLOCK = pygame.time.Clock()
 
 class Animer:
-    def __init__(self, count, sprite, scale, rotation, pos=0, rect = False):
-        self.count = count
-        self.sprite = sprite
+    def __init__(self, sprites, scale, rotation, pos=0):
+        self.sprites = sprites
         self.pos = pos
         self.rotation = rotation
-        self.load = []
-        self.transform = []
-        self.rotate = []
         self.scale = scale
-        self.rect = rect
 
-    def prep(self):
-        for i in range(0,len(self.sprite)):
-            self.load.append(pygame.image.load(self.sprite[i]))
-            self.rotate.append(pygame.transform.rotate(self.load[i], self.rotation))
-            self.transform.append(pygame.transform.scale(self.rotate[i], self.scale))
-            
-        if self.rect == True:
-            self.ent_rect = self.transform[0].get_rect()
-            return self.ent_rect
+    def prep(self, spriteList:list = None, new_rect = True):
+        self.transformed = []
+        if spriteList == None:
+            spriteList = self.sprites
+        for sprite in spriteList:
+            sprite = (pygame.image.load(sprite))
+            sprite = (pygame.transform.rotate(sprite, self.rotation))
+            sprite = (pygame.transform.scale(sprite, self.scale))
+            self.transformed.append(sprite)
 
-        return self.transform
-    
-        
-
-    def animer(self, spritesheet):
-        tick = int(72/len(spritesheet))
-        WINDOW.blit(spritesheet[self.count//tick], self.pos)
-
-
-class Fiender:
-    def __init__(self, runde):
-        self.runde = runde
+        if new_rect == True:
+            self.ent_rect = self.transformed[0].get_rect()
 
     def squid(self):
-        if self.runde == 3:
-            return prep_squid_dykk
-
-        elif self.runde == 7:
-            return prep_squid_opp   
-
-        elif self.runde >= 4:
-            return prep_squidu
-
+        if runde == 3:
+            self.prep(squid_dykk_sprites, False)
+        elif runde == 7:
+            self.prep(squid_opp_sprites, False)
+        elif runde >= 4:
+            self.prep(squidu_sprites, False)
         else:
-            return prep_squido
-    
-    def shark(self):
-       
-        if self.runde >= 4:
-            return prep_sharko
-        
+            self.prep(squido_sprites, False)
+
+    def shark(self, runde):
+        if runde >= 4:
+            self.prep(sharko_sprites, False)
         else:
-            return prep_sharku
-                  
-prep_båt = Animer(0,pirat_sprites, (120,120), 90).prep()
+            self.prep(sharku_sprites, False)
 
-prep_squido = Animer(0,squido_sprites, (60,120), 0).prep()
-prep_squid_dykk = Animer(0,squid_dykk_sprites, (60,120), 0).prep()
-prep_squid_opp = Animer(0,squid_opp_sprites, (60,120), 0).prep()
-prep_squidu = Animer(0,squidu_sprites, (60,120), 0).prep()
-
-prep_sharku = Animer(0,sharku_sprites, (60,120), 0).prep()
-prep_sharko = Animer(0,sharko_sprites, (60,120), 0).prep()
-
-
-"""
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-
-    WINDOW.fill(color_ocean)
-
-    CLOCK.tick(fps)
-
-    Animer(count, 0, 0, 0, (400, 350)).animer(Fiender(runde).shark())
-    Animer(count, 0, 0, 0, (400,500)).animer(prep_båt)
-    Animer(count, 0, 0, 0, (200,500)).animer(Fiender(runde).squid())
-
-    count +=1
-    if count >= 72:
-        count=0
-        runde += 1
-        if runde == 8:
-            runde = 0
-    
-    pygame.display.update()
-"""
+    def animer(self,count):
+        tick = int(72/len(self.transformed))
+        WINDOW.blit(self.transformed[count//tick], self.ent_rect)
--- main.py
+++ main.py
@@ -8,28 +8,58 @@ from pathlib import Path
 import random
 from grafikk import *
 
-class Enemy():
-    def __init__(self, window, windowWidth, windowHeight, speed) -> None:
+class Player():
+    def __init__(self, window, windowWidth, windowHeight, ship_width_height):
         self.window = window
         self.windowWidth = windowWidth
         self.windowHeigth = windowHeight
-        #pathToShip = str(BASE_PATH) + '/Grafikk/Pirat_spiller/Piratskip1.png'
-        #self.enemyImage = pygame.image.load(pathToShip)
-        #self.enemyImage = pygame.transform.rotate(self.enemyImage,180)
-        #self.enemyImage = pygame.transform.scale(self.enemyImage, (SHIP_WIDTH_HEIGHT, SHIP_WIDTH_HEIGHT))
-        self.enemyRect = Animer(0,sharko_sprites,(60,120),0,0, True).prep()
+        self.ship_width_height = ship_width_height
+        self.playerEntity = Animer(pirat_sprites, (ship_width_height, ship_width_height), 90, 0)
+        self.playerEntity.prep()
+        self.playerRect = self.playerEntity.ent_rect
+        self.playerRect.left = self.windowWidth/2
+        self.playerRect.top = self.windowHeigth - (self.windowHeigth/20) - ship_width_height
+        self.cannons = [(self.playerRect[2]/32*16.5, self.playerRect[3]/32*10), (self.playerRect[2]/32*19.5, self.playerRect[3]/32*10), (self.playerRect[2]/32*22.5, self.playerRect[3]/32*10)]
+
+    def shoot(self, cannon:int):
+        shootingPoint = (self.cannons[cannon][0] + self.playerRect.left, self.cannons[cannon][1] + self.playerRect.top)
+        aBullet = bullet_Gen(self.window, self.windowWidth, self.windowHeigth, shootingPoint, 5)
+        bullets.append(aBullet)
+
+    def update(self, count):
+        self.playerEntity.animer(count)
+        if (self.playerRect[2], self.playerRect[3]) != (self.ship_width_height, self.ship_width_height):
+            width_height = self.playerRect[2] + 1
+            self.playerEntity.scale = (width_height, width_height)
+            self.playerEntity.prep()
+            self.playerEntity.ent_rect = self.playerRect
+            self.playerRect[2] += 1
+            self.playerRect[3] += 1
+            self.playerRect.top = self.windowHeigth - (self.windowHeigth/20) - self.playerRect[2]
+            self.cannons = [(self.playerRect[2]/32*16.5, self.playerRect[3]/32*10), (self.playerRect[2]/32*19.5, self.playerRect[3]/32*10), (self.playerRect[2]/32*22.5, self.playerRect[3]/32*10)]
+
+    def evolve(self, size):
+        self.ship_width_height = size
+
+class Enemy():
+    def __init__(self, window, windowWidth, windowHeight, enemyWidth, enemyHeight, speed) -> None:
+        self.window = window
+        self.windowWidth = windowWidth
+        self.windowHeigth = windowHeight
+        self.enemyEntity = Animer(sharko_sprites,(enemyWidth,enemyHeight),0,0)
+        self.enemyEntity.prep()
+        self.enemyRect = self.enemyEntity.ent_rect
         self.x = 0
         self.y = - SHIP_WIDTH_HEIGHT
         self.enemyRect.top = self.y
         self.speed = speed
 
     def spawn(self, x, y):
-        #print(x,y)
         self.x = x
         self.y = y
         self.enemyRect.left = self.x
 
-    def updatePOS(self):
+    def update(self):
         ind = enemies.index(self)
         other = enemies.copy()
         other.pop(ind)
@@ -41,9 +71,10 @@ class Enemy():
         if self.enemyRect.left != self.x:
             fortegn = (self.enemyRect.left - self.x) / abs(self.enemyRect.left - self.x)
             self.enemyRect.left = self.enemyRect.left + (self.speed * fortegn)
-    
-    def draw(self):
-        Animer(count, 0, 0, 0, self.enemyRect).animer(Fiender(runde).shark())
+
+    def draw(self, count, runde):
+        self.enemyEntity.shark(runde)
+        self.enemyEntity.animer(count)
 
 class Explosion():
     def __init__(self, window, windowWidth, windowHeight, x, y, radius):
@@ -54,40 +85,43 @@ class Explosion():
         self.y = y
         self.radius = radius
         self.curRadius = 1
-
-    def spawn(self):
-        self.circle = pygame.draw.circle(self.window, EXPLOSION1, (self.x, self.y), 1)
+        self.minus = radius/5
 
     def update(self):
-        if self.circle != self.curRadius:
+        if self.radius != self.curRadius:
             self.circle = pygame.draw.circle(self.window, EXPLOSION1, (self.x, self.y), self.radius-self.curRadius)
-            self.circle = pygame.draw.circle(self.window, EXPLOSION2, (self.x, self.y), self.radius-self.curRadius-1)
-            self.circle = pygame.draw.circle(self.window, EXPLOSION3, (self.x, self.y), self.radius-self.curRadius-2)
-            self.circle = pygame.draw.circle(self.window, EXPLOSION4, (self.x, self.y), self.radius-self.curRadius-3)
-            self.circle = pygame.draw.circle(self.window, EXPLOSION5, (self.x, self.y), self.radius-self.curRadius-4)
+            self.circle = pygame.draw.circle(self.window, EXPLOSION2, (self.x, self.y), self.radius-self.curRadius-self.minus)
+            self.circle = pygame.draw.circle(self.window, EXPLOSION3, (self.x, self.y), self.radius-self.curRadius-(self.minus*2))
+            self.circle = pygame.draw.circle(self.window, EXPLOSION4, (self.x, self.y), self.radius-self.curRadius-(self.minus*3))
+            self.circle = pygame.draw.circle(self.window, EXPLOSION5, (self.x, self.y), self.radius-self.curRadius-(self.minus*4))
             self.curRadius = self.curRadius + 1
         else:
-            del self
+            explosions.remove(self)
 
 class bullet_Gen():
 
-    def __init__(self, window, windowWidth, windowHeight, ship, speed):
+    def __init__(self, window, windowWidth, windowHeight, pos, speed):
         self.window = window
         self.windowWidth = windowWidth
         self.windowHeigth = windowHeight
-        self.ship = ship
+        self.pos = pos
         self.speed = speed
         self.radius = 1
 
-        self.x = self.ship[0] + (self.ship[2]/32*(32-12))
-        self.y = self.ship[1] + (self.ship[3]/32*9)
-
+        self.x = pos[0]
+        self.y = pos[1]
 
         self.circle = pygame.draw.circle(self.window, BLACK, (self.x, self.y), self.radius)
 
     def update(self):
         self.y = self.y - self.speed
-
+        print(self.y)
+        if self.y < 0 - self.radius:
+            bullets.remove(self)
+        elif any([bullet.circle.colliderect(enemy.enemyRect) for enemy in enemies]):
+            anExplosion = Explosion(window, WINDOW_WIDTH, WINDOW_HEIGHT, bullet.x, bullet.y, 40)
+            explosions.append(anExplosion)
+            bullets.remove(self)
 
     def draw(self):
         self.circle = pygame.draw.circle(self.window, BLACK, (self.x, self.y), self.radius)
@@ -107,10 +141,11 @@ OCEAN_COLOR = (118, 200, 213)
 WINDOW_WIDTH = 1366
 WINDOW_HEIGHT = 768
 FRAMES_PER_SECOND = 28
-SHIP_WIDTH_HEIGHT = 100
+SHIP_WIDTH_HEIGHT = 96
+ENEMY_WIDTH = 64
+ENEMY_HEIGHT = 96
 MAX_WIDTH = WINDOW_WIDTH - SHIP_WIDTH_HEIGHT
 MAX_HEIGHT = WINDOW_HEIGHT - SHIP_WIDTH_HEIGHT
-TARGET_WIDHT_HEIGHT = 120
 N_PIXELS_TO_MOVE = WINDOW_WIDTH / 200
 
 # 3 - Initialize the world
@@ -119,15 +154,13 @@ window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 clock = pygame.time.Clock()
  
 # 4 - Load assets: image(s), sound(s),  etc.
-pathToShip = str(BASE_PATH) + '/Grafikk/Pirat_spiller/Piratskip1.png'
-shipImage = pygame.image.load(pathToShip)
-shipImage = pygame.transform.rotate(shipImage,90)
-shipImage = pygame.transform.scale(shipImage, (SHIP_WIDTH_HEIGHT, SHIP_WIDTH_HEIGHT))
+
 
 # 5 - Initialize variables
-shipRect = shipImage.get_rect()
-shipRect.left = WINDOW_WIDTH/2
-shipRect.top = WINDOW_HEIGHT - (WINDOW_HEIGHT/20) - SHIP_WIDTH_HEIGHT
+player = Player(window, WINDOW_WIDTH, WINDOW_HEIGHT, SHIP_WIDTH_HEIGHT)
+#shipRect = shipImage.get_rect()
+#shipRect.left = WINDOW_WIDTH/2
+#shipRect.top = WINDOW_HEIGHT - (WINDOW_HEIGHT/20) - SHIP_WIDTH_HEIGHT
 bullets = []
 enemies = []
 explosions = []
@@ -139,7 +172,7 @@ while True:
     # 7 - Check for and handle events
     for event in pygame.event.get():
         # Clicked the close button? Quit pygame and end the program 
-        if event.type == pygame.QUIT:           
+        if event.type == pygame.QUIT:    
             pygame.quit()  
             sys.exit()
 
@@ -147,55 +180,41 @@ while True:
     keyPressedTuple = pygame.key.get_pressed()
 
     if keyPressedTuple[pygame.K_LEFT]:
-        shipRect.left = shipRect.left - N_PIXELS_TO_MOVE
+        player.playerRect.left = player.playerRect.left - N_PIXELS_TO_MOVE
     if keyPressedTuple[pygame.K_RIGHT]:
-        shipRect.left = shipRect.left + N_PIXELS_TO_MOVE
+        player.playerRect.left = player.playerRect.left + N_PIXELS_TO_MOVE
     if keyPressedTuple[pygame.K_SPACE]:
-        aBullet = bullet_Gen(window, WINDOW_WIDTH, WINDOW_HEIGHT, shipRect, 5)
-        bullets.append(aBullet)
+        player.shoot(2)
     if keyPressedTuple[pygame.K_UP]:
-        aEnemy = Enemy(window, WINDOW_WIDTH, WINDOW_HEIGHT, 2)
-        aEnemy.spawn(random.randint(SHIP_WIDTH_HEIGHT/2, WINDOW_WIDTH-SHIP_WIDTH_HEIGHT), 200)
+        aEnemy = Enemy(window, WINDOW_WIDTH, WINDOW_HEIGHT, ENEMY_WIDTH, ENEMY_HEIGHT, 2)
+        aEnemy.spawn(random.randint(ENEMY_WIDTH/2, WINDOW_WIDTH-ENEMY_WIDTH), 200)
         enemies.append(aEnemy)
+    if keyPressedTuple[pygame.K_c]:
+        player.evolve(200)
 
     for bullet in bullets:
         bullet.update()
     for enemy in enemies:
-        enemy.updatePOS()
-    for explosion in explosions:
-        explosions.update()
-
-
-    #if ballRect.colliderect(targetRect):
-    #    print('BALL IS TOUCHING THE TARGET!')
-    #else:
-    #    print('ball is not touching the target')
+        enemy.update()
     
     # 9 - Clear the window
     window.fill(OCEAN_COLOR)
     
     # 10 - Draw all window elements
-    # draw ball at position 100 across (x) and 200 down (y)
-    #window.blit(targetImage, (TARGET_X, TARGET_Y))
+    player.update(count)
     for bullet in bullets:
-        if bullet.y < 0:
-            del bullet
-        elif any([bullet.circle.colliderect(enemy.enemyRect) for enemy in enemies]):
-            anExplosion = Explosion(window, WINDOW_WIDTH, WINDOW_HEIGHT, bullet.x, bullet.y, 5)
-            del bullet
-        else:
-            bullet.draw()
+        bullet.draw()
     for enemy in enemies:
-        enemy.draw()
-    #window.blit(shipImage, shipRect)
-    Animer(count, 0, 0, 90, shipRect).animer(prep_båt)   
+        enemy.draw(count, runde)
+    for explosion in explosions:
+        explosion.update()
 
     count +=1
     if count >= 72:
         count=0
         runde += 1
         if runde == 8:
-            runde = 0 
+            runde = 0
     
 
     # 11 - Update the window
